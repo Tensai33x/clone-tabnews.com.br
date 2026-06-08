615b94425e54dfd99b32506c61b3db9cae729a7a
Rian Andrade <spaikesamtll@gmail.com>
2026-06-08 17:48:48 +0000

    add `js.config.json` and improve run dev to run database

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,7 +1,3 @@
 import database from "../../../../infra/database.js";
 
-export default async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1;");
-  console.log(result.rows);
-  response.status(200).json({ chave: "Oi mano raians" });
-}
+export default async function status(request, response) {}
