c7b3ce02debee2b8aedfcb4db896ff0e088881c5
Rian Andrade <spaikesamtll@gmail.com>
2026-06-17 04:20:28 +0000

    add migrations config

--- infra/database.js
+++ infra/database.js
@@ -8,7 +8,6 @@ async function query(queryObject) {
     database: process.env.PGDATABASE,
     password: process.env.PGPASSWORD,
     ssl: process.env.NODE_ENV === "development" ? false : true,
-    
   });
 
   try {
